b32be4ea172d308174515a43c6e9b01c48dbc7dd
riel <176800746+rielThy@users.noreply.github.com>
2026-02-16 04:14:11 +0800

    Add temperature conversion between Celsius and Fahrenheit

---
+++ Temperature Calculator.py
@@ -0,0 +1,11 @@
+unit = str(input("Is this temp in Celcius or Fahrenheit [C/F]: "))
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f"The temp in Fahrenheit is: {temp}F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temp in Celsius is: {temp}C")
+else:
+    print(f"{unit} is an invalid unit of measurement")
